95c756a0bd7b4bc389bb3f0597377b753bfc5592
HEun0420 <usialeta@gmail.com>
2024-10-17 18:22:06 +0900

    fastapi

---
+++ 08_todo/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, Boolean, Text
+from database import Base
+
+# 데이터베이스에 저장될 Todo 테이블 만들기
+
+class Todo(Base):
+    # 테이블이름 설정
+    __tablename__ = 'todos'
+    # 컬럼 설정
+    id = Column(Integer, primary_key= True)
+    task = Column(Text)
+    complete = Column(Boolean, default=False)
